3537edcc6902066c5ef889c30c625603147b16f9
Sudhir Vaghela <sudhirkumar.vaghela@brainvire.com>
2023-03-31 12:07:29 +0530

    added helper classed

---
+++ MVVMDemo/Modules/Helper/Localized.swift
@@ -0,0 +1,14 @@
+//
+//  Localized.swift
+//  MVVMDemo
+//
+//  Created by Sudhir Vaghela on 31/03/23.
+//
+
+import Foundation
+
+final class Localized {
+	static var bundle: Bundle {
+		Bundle(for: Localized.self)
+	}
+}
---
+++ MVVMDemo/Modules/Helper/MainQueueDispatchDecorator.swift
@@ -0,0 +1,24 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  MVVMDemo
+//
+//  Created by Sudhir Vaghela on 31/03/23.
+//
+
+import Foundation
+
+final class MainQueueDispatchDecorator<T> {
+	internal let decoratee: T
+	
+	init(decoratee: T) {
+		self.decoratee = decoratee
+	}
+	
+	func dispatch(completion: @escaping () -> Void) {
+		guard Thread.isMainThread else {
+			return DispatchQueue.main.async(execute: completion)
+		}
+		
+		completion()
+	}
+}
